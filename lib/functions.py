524673e4214ecdfdde347420f71d4dbf98a52c19
Mati Joseph <crucialniccur@gmail.com>
2025-05-05 15:08:03 +0300

    define greet_programmer function

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+greet_programmer()
+
 
 def greet(name):
     pass
 
+
 def greet_with_default(name="programmer"):
     pass
 
+
 def add(num1, num2):
     pass
+
 
 def halve(number):
     pass
